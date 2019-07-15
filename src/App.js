ce2b8fc046788b4cda9726baa919a292a9eb6724
Svetlana Alekseenko <s.alekseenko@everad.com>
2019-07-15 15:53:58 +0300

    add event onChange nad style cards

--- src/App.js
+++ src/App.js
@@ -7,23 +7,29 @@ const App = props => {
     persons: [{ name: "Sveta", age: 24 }, { name: "Dima", age: 25 }]
   });
 
-  const clickButtonHandler = () => {
+  const clickButtonHandler = (newName) => {
     setPerssonState({
-      persons: [{ name: "Svitlana", age: 24 }, { name: "Dima", age: 26 }]
+      persons: [{ name: newName, age: 24 }, { name: "Dima", age: 26 }]
     });
   };
+  const nameChangedHandler = (event) => {
+    setPerssonState({
+      persons: [{ name: "Sveta", age: 24 }, { name: event.target.value, age: 26 }]
+    });
+  }
+
   return (
     <div className="App">
       <h1>Hi!</h1>
-      <button onClick={clickButtonHandler}>Swiches</button>
+      <button className="Button" onClick={clickButtonHandler.bind(this, 'Svitlana')}>Swiches</button>
       <Person
         name={personState.persons[0].name}
-        age={personState.persons[0].age}
-      />
+        age={personState.persons[0].age} />
       <Person
         name={personState.persons[1].name}
         age={personState.persons[1].age}
-      >
+        click={clickButtonHandler.bind(this, 'Sveta!!!')}
+        changed={nameChangedHandler.bind(this)}>
         His hobby
       </Person>
     </div>
