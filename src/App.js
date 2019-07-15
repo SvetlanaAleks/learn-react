49e74990727e518ff03af4d8f7f96d8e13ad2ca0
Svetlana Alexeyenko <sv.alexeyenko@gmail.com>
2019-07-15 08:17:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
-function App() {
+const App = props => {
+  const [personState, setPerssonState] = useState({
+    persons: [{ name: "Sveta", age: 24 }, { name: "Dima", age: 25 }]
+  });
+
+  const clickButtonHandler = () => {
+    setPerssonState({
+      persons: [{ name: "Svitlana", age: 24 }, { name: "Dima", age: 26 }]
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi!</h1>
+      <button onClick={clickButtonHandler}>Swiches</button>
+      <Person
+        name={personState.persons[0].name}
+        age={personState.persons[0].age}
+      />
+      <Person
+        name={personState.persons[1].name}
+        age={personState.persons[1].age}
+      >
+        His hobby
+      </Person>
     </div>
   );
-}
+};
 
 export default App;
