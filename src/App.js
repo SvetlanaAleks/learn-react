a9da01eb1851cdab304a9b4218b618c23b1404d5
Svetlana Alekseenko <s.alekseenko@everad.com>
2019-07-22 11:59:01 +0300

    add all

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import Person from './Person/Person';
-// import Validation from './Validation/Validation';
-// import Char from './Char/Char';
+
 
 class App extends Component {
   state = {
@@ -49,7 +49,7 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
     };
 
     let persons = null;
@@ -69,12 +69,17 @@ class App extends Component {
       );
       style.backgroundColor = 'red';
     }
-
+    let classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
     return (
-
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
